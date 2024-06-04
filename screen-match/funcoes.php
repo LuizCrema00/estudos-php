8359850db08b1b55bf933d3abdc71cbeadf4d1db
Luiz Guilherme Crema <lgcrema@hotmail.com>
2024-06-04 19:37:04 -0300

    Separando arquivos e funcoes

---
+++ screen-match/funcoes.php
@@ -0,0 +1,15 @@
+<?php
+
+function exibeMensagemLancamento(int $ano): void {
+    if ($ano > 2022) {
+        echo "Esse filme é um lancamento", PHP_EOL;
+    } elseif ($ano > 2020 && $ano <= 2022) {
+        echo "Esse filme ainda é novo", PHP_EOL;
+    } else {
+        echo "Esse filme nao é um lancamento", PHP_EOL;
+    }
+}
+
+function incluidoNoPlano(bool $planoPrime, int $anoLancamento): bool {
+    return $planoPrime || $anoLancamento < 2020;
+}
---
+++ screen-match/screen-match.php
@@ -0,0 +1,58 @@
+<?php
+
+require __DIR__ . "/funcoes.php"; //__DIR__ é como se fosse uma abreviacao para fornecer o caminho absoluto para o arquivo
+
+echo 'Bem vindo ao screen match!', PHP_EOL;
+
+$nomeFilme = "Top Gun - Maverick";
+
+$anoLancamento = 2022; 
+
+
+$quantidadeDeNotas = $argc - 1;
+$notas = [];
+
+for ($contador = 1; $contador < $argc; $contador++) {
+    $notas[] = (float) $argv[$contador];
+}
+
+$notaFilme = array_sum($notas) / $quantidadeDeNotas;
+
+$planoPrime = true; 
+$incluidoNoPlano = incluidoNoPlano($planoPrime, $anoLancamento);
+
+echo "Nome do Filme: ". $nomeFilme, PHP_EOL; 
+echo "Nota do filme: $notaFilme", PHP_EOL; 
+echo "Ano de lancamento: $anoLancamento", PHP_EOL;
+
+exibeMensagemLancamento($anoLancamento);
+
+$genero = match ($nomeFilme) {
+    "Top Gun - Maverick" => "Acao",
+    "Thor: Ragnarok" => "Herói",
+    "Se beber nao case" => "Comédia",
+    default => "Genero desconhecido"
+};
+
+echo "O genero do filme é : $genero", PHP_EOL;
+
+$filme = [
+    "nome" => "Thor: Ragnarok",
+    "Ano" => 2021,
+    "nota" => 7.8,
+    "genero" => "super-herói"
+];
+
+var_dump($notas); //acessa o array notas e mostras seus valores e seus tipos
+sort($notas); //ordena o array notas em ordem crescente
+var_dump($notas);
+$menorNota = min($notas); //obtem a menor nota do array notas e armazena na variavel menorNota
+echo $menorNota, PHP_EOL;
+
+var_dump($filme['nome']); //mostra a string do indice nome, do array armazenado na variavel $filme
+$posicaoDoisPontos = strpos($filme['nome'], ':'); //obtem a posicao do ':' da string nome, e armazena na variavel $posicaoDoisPontos
+var_dump($posicaoDoisPontos);// mostra o valor armazenado na variavel $posicaoDoisPontos assim como seu tipo
+
+var_dump(substr($filme['nome'], 0, $posicaoDoisPontos)); //mostra uma substring a partir da funcao substr, essa funcao espera pelos parametros da string a ser acessada, aonde ela comeca e aonde ela termina
+
+
