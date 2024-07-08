a62ac79bee679fa5b7f774ab6113255f9ae6f002
Luiz Guilherme Crema <lgcrema@hotmail.com>
2024-07-08 13:54:59 -0300

    Prepared Statements

--- curso-pdo/inserir-aluno.php
+++ curso-pdo/inserir-aluno.php
@@ -6,8 +6,17 @@ require_once 'vendor/autoload.php';
 $caminhoBanco = __DIR__ . '/banco.sqlite'; //caminho absoluto
 $pdo = new PDO('sqlite:' . $caminhoBanco); //conexao com o banco de dados sqlite
 
-$student = new Student(null, 'Luiz Crema', new DateTimeImmutable('2000-04-08')); //instanciacao do objeto Student
+$student = new Student(
+    null, 
+    "Maria Claudia", 
+    new DateTimeImmutable('1976-11-20')
+); //instanciacao do objeto Student
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}')"; //insercao de aluno no banco sqlite
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date)"; //insercao de aluno no banco sqlite
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 
-var_dump($pdo->exec($sqlInsert)); //método exec retorna o número de linhas afetadas
+if ($statement->execute()) {
+    echo "Aluno incluído";
+}
