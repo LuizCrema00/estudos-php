231e21bbeb0dea85558d0803cd3519c5d55f137a
Luiz Guilherme Crema <lgcrema@hotmail.com>
2024-07-16 14:50:10 -0300

    Refactor: alterando metodo de get para post em excluir produto

--- serenatto-cafe/excluir-produto.php
+++ serenatto-cafe/excluir-produto.php
@@ -5,5 +5,6 @@ require "src/Model/Produto.php";
 require "src/Repository/ProdutoRepository.php";
 
 $produtoRepository = new ProdutoRepository($pdo);
-$produtoRepository->deletar($_GET['id']);
+$produtoRepository->deletar($_POST['id']);
 
+header("Location: admin.php");
