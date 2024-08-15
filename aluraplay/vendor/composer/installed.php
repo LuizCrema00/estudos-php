f531feb6bfd0aaa2745ece9dc8277a6f410d056f
Luiz Guilherme Crema <lgcrema@hotmail.com>
2024-08-15 14:32:26 -0300

    atualizacao composer.lock

---
+++ aluraplay/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4aaf6011384a2330ed998b920aa3d2247875c0cf',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4aaf6011384a2330ed998b920aa3d2247875c0cf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
