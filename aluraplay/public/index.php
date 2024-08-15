d1a84dc6557fe12718e6bbe6fe8a4c8ec3605c8b
Luiz Guilherme Crema <lgcrema@hotmail.com>
2024-08-15 11:24:03 -0300

    Adicionando mais controllers

--- aluraplay/public/index.php
+++ aluraplay/public/index.php
@@ -1,23 +1,39 @@
 <?php
+use Alura\Mvc\Controller\{
+    Controller,
+    DeleteVideoController,
+    EditVideoController,
+    Error404Controller,
+    NewVideoController,
+    VideoFormController,
+    VideoListController
+};
+use Alura\Mvc\Repository\VideoRepository;
 
-require_once __DIR__ . '/../vendor//autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Criar a instância de PDO e VideoRepository
+$pdo = new PDO('mysql:host=localhost;dbname=alura-play', 'root', 'Crema123@');
+$videoRepository = new VideoRepository($pdo);
 
 if (!array_key_exists('PATH_INFO', $_SERVER) || $_SERVER['PATH_INFO'] === '/') {
-    require_once __DIR__ . '/../listagem-videos.php';
-} elseif ($_SERVER['PATH_INFO'] === '/novo-video'){
+    $controller = new VideoListController($videoRepository);
+} elseif ($_SERVER['PATH_INFO'] === '/novo-video') {
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        require_once __DIR__ . '/../formulario.php';
-    } elseif ($_SERVER ['REQUEST_METHOD'] === 'POST'){
-            require_once __DIR__ . '/../novo-video.php';
+        $controller = new VideoFormController($videoRepository);
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $controller = new NewVideoController($videoRepository);
     }
 } elseif ($_SERVER['PATH_INFO'] === '/editar-video') {
-    if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-        require_once __DIR__ . '/../formulario.php';
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST'){
-        require_once __DIR__ . '/../editar-video.php';
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $controller = new VideoFormController($videoRepository);
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $controller = new EditVideoController($videoRepository);
     }
 } elseif ($_SERVER['PATH_INFO'] === '/remover-video') {
-    require_once __DIR__ . '/../remover-video.php';
+    $controller = new DeleteVideoController($videoRepository);
 } else {
-    http_response_code(404);
+    $controller = new Error404Controller();
 }
+/** @var Controller $controller */
+$controller->processaRequisicao();
