c2536f888095378cc173201f67d0e29a6c376ccb
Luiz Guilherme Crema <lgcrema@hotmail.com>
2024-08-21 13:41:16 -0300

    fix

--- aluraplay/src/Controller/LoginController.php
+++ aluraplay/src/Controller/LoginController.php
@@ -25,15 +25,15 @@ class LoginController implements Controller
         $userData = $statement->fetch(PDO::FETCH_ASSOC);
         $correctPassword = password_verify($senha, $userData['senha'] ?? '');
 
-        if (password_needs_rehash($userData['password'], PASSWORD_ARGON2ID)) {
-            $statement = $this->pdo->prepare('UPDATE users SET password = ? WHERE id = ?');
-            $statement->bindValue(1, password_hash($senha, PASSWORD_ARGON2ID));
-            $statement->bindValue(2, $userData['id']);
-            $statement->execute();
-        }
-
+       
         if($correctPassword) {
             $_SESSION['logado'] = true;
+            if (password_needs_rehash($userData['senha'], PASSWORD_ARGON2ID)) {
+                $statement = $this->pdo->prepare('UPDATE users SET senha = ? WHERE id = ?');
+                $statement->bindValue(1, password_hash($senha, PASSWORD_ARGON2ID));
+                $statement->bindValue(2, $userData['id']);
+                $statement->execute();
+            }
             header('Location: /');
         } else {
             header('Location: /login?sucesso=0');
