b0864f035d12c554b653c7d4aa0e848d70bd72ab
Luiz Guilherme Crema <lgcrema@hotmail.com>
2024-08-21 13:39:41 -0300

    Seguranca com senhas

--- aluraplay/src/Controller/LoginController.php
+++ aluraplay/src/Controller/LoginController.php
@@ -25,6 +25,13 @@ class LoginController implements Controller
         $userData = $statement->fetch(PDO::FETCH_ASSOC);
         $correctPassword = password_verify($senha, $userData['senha'] ?? '');
 
+        if (password_needs_rehash($userData['password'], PASSWORD_ARGON2ID)) {
+            $statement = $this->pdo->prepare('UPDATE users SET password = ? WHERE id = ?');
+            $statement->bindValue(1, password_hash($senha, PASSWORD_ARGON2ID));
+            $statement->bindValue(2, $userData['id']);
+            $statement->execute();
+        }
+
         if($correctPassword) {
             $_SESSION['logado'] = true;
             header('Location: /');
